16f5a68053a086699548b66e0921f6bb0c0ff73e
IvanTerekh <Terekh.Ivan.Sergeevich@gmail.com>
2017-04-28 15:48:25 +0300

    UsersController made

--- IvanTerekh/Portfl/src/main/java/com/terekh/portfl/controller/UsersController.java
+++ IvanTerekh/Portfl/src/main/java/com/terekh/portfl/controller/UsersController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.terekh.portfl.model.User;
 import com.terekh.portfl.service.UserService;
@@ -17,9 +17,12 @@ public class UsersController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping("/find")
-	public String showUserPage(@RequestParam String username, Model model){
+	@GetMapping("/{username}")
+	public String showUserPage(@PathVariable String username, Model model){
 		User user = userService.findByUsername(username);
+		if (user == null){
+			return "users/404";
+		}
 		model.addAttribute("user", user);
 		return "/users/user";
 	}
